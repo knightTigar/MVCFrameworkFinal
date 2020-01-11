ebaa75480af27134fcb0f401fb84f2463c1fbc1e
sevenbitsAbhishek <patel.abhishek@tristonsoft.com>
2020-01-11 11:42:11 +0530

    First commit of code

---
+++ Src/MVCFramework/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCFramework
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //order of the routs matters 
+
+            routes.MapMvcAttributeRoutes();
+
+            /*// we create custom route
+            routes.MapRoute(
+                "MovieByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { Controller = "Movies" , action = "ByReleaseDate"},
+                new { year = @"\d{4}", month = @"\d{2}"});// @ sign is bcoz of \
+*/
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
